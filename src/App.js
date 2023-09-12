bb9aa5a3881374b29628d27aa967d247d1e9aa15
Gabriel Coronel <gabriel.c1901@gmail.com>
2023-09-12 12:44:14 -0300

    add title and input components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Title } from "./components/Title";
+import { TodoInput } from "./components/TodoInput";
 
 function App() {
   return (
     <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
-      <div className="container flex flex-col max-w-xl"></div>
+      <div className="container flex flex-col max-w-xl">
+        <Title />
+        <TodoInput />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/index.html", "./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+        display: ["Dancing Script", "cursive"],
+        inter: "'Inter', serif",
+      },
+    },
   },
   plugins: [],
 };
